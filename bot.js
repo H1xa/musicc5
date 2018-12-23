c21164a5aa648520330ff2759c3c363d99394752
H1xa <45233274+H1xa@users.noreply.github.com>
2018-12-23 21:46:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By | Rqmz ' `);
     console.log(`══════════ஜ۩۞۩ஜ════════════  `);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`5play | Last Music`,"http://twitch.tv/Rqmz Shop")
+client.user.setGame(`5play | Rqmz Music`,"http://twitch.tv/Rqmz Shop")
 client.user.setStatus("dnd")
 });
 
